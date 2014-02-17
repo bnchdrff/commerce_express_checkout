47a720c354330a2b658072c8805d11454ee955ec
Benjamin Chodoroff <ben@theworkdept.com>
2014-02-16 20:38:28 -0500

    custom quantities

---
+++ commerce_express_checkout.custom_qty.js
@@ -0,0 +1,15 @@
+(function($) {
+
+Drupal.behaviors.cec_custom_qty = {
+  attach: function(context, settings) {
+    $('.qty_ctl', context).on('keydown keyup click input submit mouseenter', function(ev) {
+      var qty = this.value;
+      var $a = $(this).siblings('a').eq(0);
+      var $a_href = $a.attr('href');
+      console.log('replacing things');
+      $a.attr('href', $a_href.replace(/(.*commerce-express-checkout\/\d*)\/\d*/, '$1/' + qty));
+    });
+  }
+};
+
+})(jQuery);
