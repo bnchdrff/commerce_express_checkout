511336c00976b60384be1125cc9bf2cf0d1e2f82
Benjamin Chodoroff <ben@theworkdept.com>
2014-04-11 22:01:05 +0000

    oops a console.log

--- commerce_express_checkout.custom_qty.js
+++ commerce_express_checkout.custom_qty.js
@@ -6,7 +6,6 @@ Drupal.behaviors.cec_custom_qty = {
       var qty = this.value;
       var $a = $(this).siblings('a').eq(0);
       var $a_href = $a.attr('href');
-      console.log('replacing things');
       $a.attr('href', $a_href.replace(/(.*commerce-express-checkout\/\d*)\/\d*/, '$1/' + qty));
     });
   }
