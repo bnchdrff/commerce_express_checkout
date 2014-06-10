b72551f6b5584d309b922c3c6c3525270610a29c
Benjamin Chodoroff <ben@theworkdept.com>
2014-06-10 21:19:42 +0000

    Issue #1677676: Themable button

---
+++ commerce_express_checkout_button.tpl.php
@@ -0,0 +1,8 @@
+<?
+/**
+ * $link - link to express checkout of current product;
+ */
+?>
+<span style="display: inline-block; padding: 10px; border: 1px solid #CCCCCC; border-radius: 20px; background-color: #EEEEEE;">
+  <a href="<?php print $link; ?>" style="text-decoration: none; font-weight: bold; color: #666666"><?php print t('Express Checkout'); ?></a>
+</span>
